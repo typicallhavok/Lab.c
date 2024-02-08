3525520bffbfad3a54c89a970ae7e894d3259dc6
typicallhavok <diamondpineappleboy@gmail.com>
2024-02-08 22:12:39 +0530

    Update L2.c

--- L2.c
+++ L2.c
@@ -5,10 +5,9 @@ int main()
 {
     int a = 5, b = 6;
     printf("Before swapping: %d and %d\n", a, b);
-    b += a;
-    a -= 2 * a;
     a += b;
-    b -= a;
+    b = a - b;
+    a -= b;
     printf("After swapping : %d and %d\n", a, b);
     return 0;
 }
